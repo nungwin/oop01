daae96025ac5d80440f9907145868f70ac68d29a
nungwin <infinix2529@gmail.com>
2026-06-17 16:10:39 +0700

    ส่งงาน Assignment1_1

---
+++ student.ts
@@ -0,0 +1,17 @@
+let fname: string = "Salawin";
+let lname: string = "Janaijang";
+let course: string[] = ["database","oop","Web"];
+let mark: number[] = [80,76,65];
+let pass: boolean = true;
+console.log("hello %s %s คุณได้ %d คะเเนน",fname,lname,mark);
+console.log("ลงทะเบียนเรียนวิชา",course);
+console.log("ได้คะเเนน",mark);
+console.log("คุณสอบผ่าน",pass);
+
+console.log("คุณสอบได้ %d คะเเนน วิชา %s",mark[0],course[0]);
+
+let mark1: number=75;
+let mark2: number=80;
+
+let total: number=mark[0]+mark[1]+mark[2];
+console.log("คะเเนนรวม",total);

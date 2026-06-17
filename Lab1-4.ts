aefc1ca28409d5f89de8ab4289c4e701f30f272d
nungwin <infinix2529@gmail.com>
2026-06-17 16:58:02 +0700

    ส่งงาน lab1_4

---
+++ Lab1-4.ts
@@ -0,0 +1,11 @@
+let income: number = 50000;
+let taxRate: number = 7;
+let Tax: number =income*0.07;
+let Net: number =income - income*0.07;
+
+console.log("---Tax Calculator Rrport---");
+console.log("Gross income: %d",income);
+console.log("Tax rate 7%");
+console.log("Tax paid: %d",Tax);
+console.log("Net income after tax: %d",Net);
+console.log("----------------------------");
